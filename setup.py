5b8caf6cc44455732315d28a482221df9b24ae72
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2018-07-07 19:31:03 +0900

    trivial

--- setup.py
+++ setup.py
@@ -11,7 +11,12 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', ]
+requirements = [
+    'Click>=6.0',
+    'spacy>=2.0,<3.0',
+    'ujson',
+    'click'
+]
 
 setup_requirements = ['pytest-runner', ]
 
@@ -30,15 +35,15 @@ setup(
     description="Entity recognizer for Exobrain project.",
     entry_points={
         'console_scripts': [
-            'exobrain_entity=exobrain_entity.cli:main',
+            'exobrain_entity_recognizer=exobrain_entity_recognizer.cli:main',
         ],
     },
     install_requires=requirements,
     long_description=readme + '\n\n' + history,
     include_package_data=True,
-    keywords='exobrain_entity',
-    name='exobrain_entity',
-    packages=find_packages(include=['exobrain_entity']),
+    keywords='exobrain_entity_recognizer',
+    name='exobrain_entity_recognizer',
+    packages=find_packages(include=['exobrain_entity_recognizer']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
