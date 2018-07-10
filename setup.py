5e819d39dcd256b42897cd822de2343cfbd90327
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2018-07-10 13:33:23 +0900

    add support for python 3.7

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
     ],
     description="Entity recognizer for Exobrain project.",
     entry_points={
