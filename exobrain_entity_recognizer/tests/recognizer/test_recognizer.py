94284bf8eb373447eec4ed1f0f9ddf79443cd90b
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2018-07-10 10:34:10 +0900

    trivial

--- exobrain_entity_recognizer/tests/recognizer/test_recognizer.py
+++ exobrain_entity_recognizer/tests/recognizer/test_recognizer.py
@@ -12,23 +12,29 @@ from ...recognizer import EntityRecognizer
 from ... import cli
 import spacy
 
-SPACY_MODEL = 'en'
 
-
-@pytest.fixture(scope="module")
+@pytest.fixture(scope='session')
 def models():
-    call(['python', '-m', 'spacy', 'download', SPACY_MODEL])
+    call(['python', '-m', 'spacy', 'download', 'en'])
     call(['python', '-m',  'textblob.download_corpora'])
 
 
-def test_entity_recognizer(models):
-    # run recognizer
-    recognizer = EntityRecognizer(SPACY_MODEL)
-    text = """
+@pytest.fixture(scope='module')
+def recognizer(models):
+    return EntityRecognizer('en')
+
+
+@pytest.fixture(scope='module')
+def text():
+    return """
         spaCy excels at large-scale information extraction tasks. 
         It's written from the ground up in carefully memory-managed Cython. 
         Independent research has confirmed that spaCy is the fastest in the world. 
         If your application needs to process entire web dumps, spaCy is the library you want to be using.
     """
+
+
+def test_entity_recognizer(text, recognizer):
+    # run recognizer
     for ent in recognizer(text):
         assert ent != {}
