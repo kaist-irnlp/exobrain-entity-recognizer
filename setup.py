07edf55723d4ea8d3bc63385b01b25ef082881aa
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2018-07-10 13:35:17 +0900

    remove support for python 3.7 (not supported in travis)

--- setup.py
+++ setup.py
@@ -30,8 +30,7 @@ setup(
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.6'
     ],
     description="Entity recognizer for Exobrain project.",
     entry_points={
